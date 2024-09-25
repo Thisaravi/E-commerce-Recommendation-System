191d215c38e72156781ae91e2626c2529671b0dd
Thisaravi <hashinikathisaravi0523@gmail.com>
2024-09-25 20:50:05 +0530

    home and main

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+#routes
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/main")
+def main():
+    return render_template('main.html')
+
+@app.route("/index")
+def indexredirect():
+    return render_template('index.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
